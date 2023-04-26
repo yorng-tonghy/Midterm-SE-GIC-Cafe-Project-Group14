74f942848e43f81113f5229333972398dbb3f861
tonghy <tonghy@>
2023-04-26 22:21:44 +0700

    2nd Commit

---
+++ Yorng-Tonghy/src/main/java/SE_Group14_YORNGTONGHY/YorngTonghyApplication.java
@@ -0,0 +1,13 @@
+package SE_Group14_YORNGTONGHY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YorngTonghyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YorngTonghyApplication.class, args);
+	}
+
+}
---
+++ Yorng-Tonghy/src/test/java/SE_Group14_YORNGTONGHY/YorngTonghyApplicationTests.java
@@ -0,0 +1,13 @@
+package SE_Group14_YORNGTONGHY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YorngTonghyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
