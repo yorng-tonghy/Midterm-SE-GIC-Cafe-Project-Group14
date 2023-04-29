f31bbe6f2af95b741bb1e810a0a3d64adcff561f
tonghy <tonghy@>
2023-04-29 13:49:47 +0700

    committed

---
+++ GIC_Cafe/src/main/java/Group14/GIC_Cafe/ServletInitializer.java
@@ -0,0 +1,13 @@
+package Group14.GIC_Cafe;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GicCafeApplication.class);
+	}
+
+}
