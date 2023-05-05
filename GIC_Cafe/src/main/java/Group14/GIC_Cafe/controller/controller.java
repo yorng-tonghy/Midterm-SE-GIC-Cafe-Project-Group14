97f66487224ef1e9f3a81f5d05e86f49225f0181
ssopheak2002@gmail.com <ssopheak2002@gmail.com>
2023-05-06 00:53:09 +0700

    UI 3&4 and 9&10

--- GIC_Cafe/src/main/java/Group14/GIC_Cafe/controller/controller.java
+++ GIC_Cafe/src/main/java/Group14/GIC_Cafe/controller/controller.java
@@ -6,14 +6,28 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 // import org.springframework.*;
 
+import ch.qos.logback.core.model.Model;
+
 
 @RestController
 public class controller {
-    @GetMapping(path = "/cashier_section4")
-   public ModelAndView cashier_section4(){
-        return new ModelAndView("cashier_section4");
-   }
-    
-   
+     @GetMapping(path = "/cashier_section4")
+     public ModelAndView cashier_section4(){
+          return new ModelAndView("cashier_section4");
+     }
 
+     @GetMapping(path = "/drink_select_order_info")
+     public Object drink_select_order_info() {
+          return new ModelAndView("drink_select_order_info");
+     }
+
+     @GetMapping(path = "/add_edit_cashier")
+     public Object add_edit_cashier(){
+          return new ModelAndView("add_edit_cashier");
+     }
+
+     @GetMapping(path = "/categories_management")
+     public Object categories_management(){
+          return new ModelAndView("categories_management");
+     }
 }
