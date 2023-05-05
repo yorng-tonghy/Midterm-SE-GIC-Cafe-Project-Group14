fc909714ef00e56f2f32478316f2c5dc528cf6cb
Thue Naratt <thuenaratt@gmail.com>
2023-05-05 23:52:43 +0700

    done

--- GIC_Cafe/src/main/java/Group14/GIC_Cafe/controller/controller.java
+++ GIC_Cafe/src/main/java/Group14/GIC_Cafe/controller/controller.java
@@ -23,13 +23,21 @@ public class controller {
    public ModelAndView grud_products(){
         return new ModelAndView("grud_products");
    }
-    
+
    @GetMapping(path = "/cashier_section5")
    public ModelAndView cashier_section5(){
         return new ModelAndView("cashier_section5");
    }
+   @GetMapping(path = "/table_selection")
+   public ModelAndView table_selection(){
+        return new ModelAndView("table_selection");
+   }
+   @GetMapping(path = "/order_history")
+   public ModelAndView order_history(){
+        return new ModelAndView("order_history");
+   }
     // -----Your section here------- 
->>>>>>> 96e8e3ad76b1dfd1c8f7194ac582ad56b3933172
+
    
 
 }
